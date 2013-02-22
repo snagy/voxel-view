466d4971e4c914cb9780050b2c120c90d94d8adb
Max Ogden <max@maxogden.com>
2013-02-22 21:23:04 +1300

    fix bug in threejs and add readme

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var THREE = require('three')
+var THREE, temporaryPosition, temporaryVector
 
-module.exports = View
+module.exports = function(three, opts) {
+  temporaryPosition = new three.Vector3
+  temporaryVector = new three.Vector3
+  
+  return new View(three, opts)
+}
 
-function View(opts) {
+function View(three, opts) {
+  THREE = three // three.js doesn't support multiple instances on a single page
   this.fov = opts.fov || 60
   this.width = opts.width || 512
   this.height = opts.height || 512
@@ -31,11 +37,6 @@ View.prototype.bindToScene = function(scene) {
 View.prototype.getCamera = function() {
   return this.camera
 }
-
-
-var temporaryPosition = new THREE.Vector3
-  , temporaryVector = new THREE.Vector3
-
 
 View.prototype.cameraPosition = function() {
   temporaryPosition.multiplyScalar(0)
